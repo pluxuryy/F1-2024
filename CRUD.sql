f1d8bd2a5ab6251124f5ac4a399cedf578fe490f
pluxuryy <denisokulovs.99@gmail.com>
2025-05-07 13:19:01 +0300

    Initial commit

---
+++ CRUD.sql
@@ -0,0 +1,38 @@
+-- Active: 1746560294327@@127.0.0.1@3306
+
+UPDATE Drivers
+SET
+    points = (
+        SELECT SUM(
+                Bahrain + "Saudi Arabia" + Australia + Japan + China + Miami + "Emilia-Romagna" + Monaco + Canada + Spain + Austria + "Great Britain" + Hungary + Belgium + Netherlands + Italy + Azerbaijan + Singapore + "United States" + Mexico + Brazil + "Las Vegas" + Qatar + "Abu Dhabi"
+            )
+        FROM Points
+        WHERE
+            Points.driver_id = Drivers.driver_id
+    );
+
+UPDATE Drivers
+SET
+    fastest_laps = (
+        SELECT COUNT(*)
+        FROM Fastest_laps
+        WHERE
+            Fastest_laps.driver_id = Drivers.driver_id
+    );
+
+UPDATE Teams
+SET
+    team_points = (
+        SELECT SUM(points)
+        FROM Drivers
+        WHERE
+            Drivers.team_id = Teams.team_id
+    );
+
+ALTER TABLE Drivers ADD COLUMN World_Champion BOOLEAN DEFAULT False;
+
+UPDATE Drivers
+SET
+    World_Champion = True
+WHERE
+    driver_id IN (10, 16, 6);
---
+++ app.py
@@ -0,0 +1,49 @@
+
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('f1_2024.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    drivers_count = conn.execute('SELECT COUNT(*) FROM Drivers').fetchone()[0]
+    teams_count = conn.execute('SELECT COUNT(*) FROM Teams').fetchone()[0]
+    tracks_count = conn.execute('SELECT COUNT(*) FROM Tracks').fetchone()[0]
+    conn.close()
+    return render_template('index.html', drivers_count=drivers_count, 
+                           teams_count=teams_count, tracks_count=tracks_count)
+
+
+
+@app.route('/drivers')
+def drivers():
+    conn = get_db_connection()
+    drivers = conn.execute('SELECT * FROM Drivers ORDER BY points DESC').fetchall()
+    conn.close()
+    return render_template('drivers.html', drivers=drivers)
+
+
+@app.route('/teams')
+def teams():
+    conn = get_db_connection()
+    teams = conn.execute('SELECT * FROM Teams ORDER BY team_points DESC').fetchall()
+    conn.close()
+    return render_template('teams.html', teams=teams)
+
+@app.route('/tracks')
+def tracks():
+    conn = get_db_connection()
+    tracks = conn.execute('SELECT * FROM Tracks').fetchall()
+    conn.close()
+    return render_template('tracks.html', tracks=tracks)
+
+
+
+if __name__ == '__main__':
+    app.run( debug=False, use_reloader=False)
