24e013d8cba944354e452f794f8234d08836b0dc
pluxuryy <denisokulovs.99@gmail.com>
2025-05-07 13:45:43 +0300

    Bind to dynamic port for Render

--- app.py
+++ app.py
@@ -44,6 +44,9 @@ def tracks():
     return render_template('tracks.html', tracks=tracks)
 
 
+import os
 
-if __name__ == '__main__':
-    app.run( debug=False, use_reloader=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
